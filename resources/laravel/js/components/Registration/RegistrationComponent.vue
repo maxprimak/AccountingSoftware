50c6eca5ba8547ac4b8ff602834c01fd4e2b7643
maxappdev <maxym.oliynyk95@gmail.com>
2019-08-28 20:22:05 +0200

    buefy updated

--- resources/laravel/js/components/Registration/RegistrationComponent.vue
+++ resources/laravel/js/components/Registration/RegistrationComponent.vue
@@ -1,8 +1,7 @@
 <template>
     <b-steps>
-        <b-step-item label="Account"></b-step-item>
-        <b-step-item label="Profile"></b-step-item>
-        <b-step-item label="Social"></b-step-item>
+        <b-step-item label="Person"></b-step-item>
+        <b-step-item label="Company"></b-step-item>
     </b-steps>
 </template>
 
