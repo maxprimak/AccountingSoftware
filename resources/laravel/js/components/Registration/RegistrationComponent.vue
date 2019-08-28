c9cc87d8a0ed47f158e6be6a279aa05932963056
maxappdev <maxym.oliynyk95@gmail.com>
2019-08-28 20:42:06 +0200

    registration layout

--- resources/laravel/js/components/Registration/RegistrationComponent.vue
+++ resources/laravel/js/components/Registration/RegistrationComponent.vue
@@ -1,7 +1,42 @@
 <template>
     <b-steps>
-        <b-step-item label="Person"></b-step-item>
-        <b-step-item label="Company"></b-step-item>
+        <b-step-item label="Person">
+            <div class="columns is-centered">
+                <div class="column is-one-quarter">
+                    <h3 class="title has-text-centered">Add your personal info</h3>
+                    <b-field label="Your name">
+                        <b-input></b-input>
+                    </b-field>
+                    <b-field label="Your phone">
+                        <b-input></b-input>
+                    </b-field>
+                    <b-field label="Your address">
+                        <b-input></b-input>
+                    </b-field>
+                </div>
+            </div>
+        </b-step-item>
+        <b-step-item label="Company">
+            <div class="columns is-centered">
+                <div class="column is-one-quarter">
+                    <h3 class="title has-text-centered">Add your company info</h3>
+                    <b-field label="Company name">
+                        <b-input></b-input>
+                    </b-field>
+                    <b-field label="Company address">
+                        <b-input></b-input>
+                    </b-field>
+                    <b-field label="Company phone">
+                        <b-input></b-input>
+                    </b-field>
+                    <b-field label="Company currency">
+                        <b-select>
+                            <option selected>USD</option>
+                        </b-select>
+                    </b-field>
+                </div>
+            </div>
+        </b-step-item>
     </b-steps>
 </template>
 
