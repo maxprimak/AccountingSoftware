29071e336d4970d21cf539efb23c83c3628dd13e
Maxi <maxprimak1@gmail.com>
2019-12-07 10:40:58 +0100

    old routes deleted

--- routes/web.php
+++ routes/web.php
@@ -10,34 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::middleware(['auth'])->group(function () {
-
-Route::get('/', function () {
-    return view('dashboard::dashboard');
-});
-
-Route::get('/repair-orders', function () {
-    return view('orders::RepairOrders');
-});
-Route::get('/sales-orders', function () {
-    return view('orders::SalesOrders');
-});
-Route::get('/orders-settings', function () {
-    return view('orders::OrdersSettings');
-});
-Route::get('/create-repair', function () {
-    return view('orders::CreateRepairOrders');
-});
-Route::get('/create-sales', function () {
-    return view('orders::CreateSalesOrders');
-});
-
-Route::get('/home', function () {
-    return view('dashboard::dashboard');
-});
-
-});
-
-Route::get("/v2", function(){
-   return view("main");
-});
+// Route::middleware(['auth'])->group(function () {
+//
+//
+// });
+//
+// Route::get("/v2", function(){
+//    return view("main");
+// });
