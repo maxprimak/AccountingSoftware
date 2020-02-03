17e7a2f0b09c8375e85e974c90648972d590205d
maxappdev <maxym.oliynyk95@gmail.com>
2020-02-03 12:40:56 +0100

    test relist-458

--- Modules/Orders/Http/Requests/StoreRepairOrderRequest.php
+++ Modules/Orders/Http/Requests/StoreRepairOrderRequest.php
@@ -13,34 +13,62 @@ class StoreRepairOrderRequest extends FormRequest
      */
     public function rules()
     {
-      $accept_date = $this->accept_date;
-      $accept_date = str_replace('-', '/', $accept_date);
-      $yesterday = date('Y-m-d',strtotime($accept_date . "-1 days"));
-      $amount_of_devices = sizeof($this->devices);
-      $amount_of_services = 0;
-      foreach ($this->devices as $device){
-          $amount_of_services += sizeof( $device['services_id']);
-      }
-      if($amount_of_devices == 0){
-          return ['devices' => 'size:1'];
-      }
-      if($amount_of_devices > $amount_of_services){
-          return ['amount of services should be more or equal:' => 'size:'.$amount_of_devices];
-      }
         return [
             'order_type_id' => 'required|exists:order_types,id',
             'accept_date' => 'required|date|before:tomorrow',
             'branch_id' => 'required|exists:branches,id',
             'order_nr' => 'required|max:190',
             'customer_id' => 'required|exists:customers,id',
-            'devices' => 'required',
-            'price' => 'required|numeric',
+            'price' => 'required|min:1',
             'warranty_id' => 'required|exists:warranties,id',
             'discount_code_id' => 'required|exists:discount_codes,id',
-//            'order_has_device_id' => 'sometimes|exists:repair_order_has_devices,id:',
-            'deadline' => 'nullable|date|after:'.$yesterday,
+            'deadline' => 'nullable|date|after:yesterday',
             'comment' => 'nullable|max:190',
-            'prepay_sum' => 'nullable|numeric|max:'. $this->price
+            'prepay_sum' => 'nullable|numeric|max:'.$this->price,
+            'devices' => 'required|array',
+            'devices.*.submodel_id' => 'required|exists:submodels,id',
+            'devices.*.color_id' => 'required|exists:colors,id',
+            'devices.*.serial_nr' => 'required|max:190',
+            'devices.*.condition' => 'required|max:190',
+            'devices.*.status_name' => 'required|max:190|exists:order_statuses_translations,name',
+            'devices.*.status_hexcode' => 'required|max:190|exists:order_statuses,hex_code',
+            'devices.*.last_request' => 'required',
+            'devices.*.name' => 'required|max:190',
+            'devices.*.brand_name' => 'required|max:190',
+            'devices.*.model_name' => 'required|max:190',
+            'devices.*.submodel_name' => 'required|max:190',
+            'devices.*.color_hexcode' => 'required|exists:colors,hex_code',
+            'devices.*.color_name' => 'required|exists:colors,name',
+            'devices.*.services' => 'required|array',
+            'devices.*.issue_description' => 'required|max:190',
+            'devices.*.goods.*.id' => 'required|exists:goods,id',
+            'devices.*.goods.*.brand_name' => 'required|max:190',
+            'devices.*.goods.*.brand_id' => 'required|exists:brands,id',
+            'devices.*.goods.*.model_name' => 'required|max:190',
+            'devices.*.goods.*.model_id' => 'required|exists:models,id',
+            'devices.*.goods.*.submodel_name' => 'required|max:190',
+            'devices.*.goods.*.submodel_id' => 'required|exists:submodels,id',
+            'devices.*.goods.*.part_name' => 'required|max:190',
+            'devices.*.goods.*.part_id' => 'required|exists:parts,id',
+            'devices.*.goods.*.color_name' => 'required|max:190|exists:colors,name',
+            'devices.*.goods.*.color_id' => 'required|exists:colors,id',
+            'devices.*.goods.*.color_hexcode' => 'required|exists:colors,hex_code',
+            'devices.*.goods.*.warehouse_has_good_id' => 'required|exists:warehouse_has_goods,id',
+            'devices.*.goods.*.vendor_code' => 'required|max:190',
+            'devices.*.goods.*.amount' => 'required|numeric|min:1',
+            'devices.*.goods.*.order_amount' => 'required|numeric|min:1|',
+            'devices.*.goods.*.warehouse_name' => 'required|exists:warehouses,name',
+            'devices.*.warehouse_has_good.*.id' => 'required',
+            'devices.*.warehouse_has_good.*.amount' => 'required|min:1',
+        ];
+    }
+
+    public function messages()
+    {   
+    return [
+        'devices.required' => 'order does not contain any device',
+        'devices.*.services.required' => 'some devices do not have any service',
+        'devices.*.issue_description.required' => 'some devices do not have issue description'
         ];
     }
 
