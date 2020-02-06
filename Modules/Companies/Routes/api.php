1915216956545a26ecddfcadb61dfa8728b1658a
maxappdev <maxym.oliynyk95@gmail.com>
2020-02-06 16:26:55 +0100

    registration basics

--- Modules/Companies/Routes/api.php
+++ Modules/Companies/Routes/api.php
@@ -13,19 +13,23 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::middleware(['is_authorized'])->group(function () {
+
+Route::get('cities', 'CitiesController@index')->name('cities.index');
+Route::get('currencies', 'CurrenciesController@index')->name('currencies.index');
+
+});
+
+
 Route::middleware(['is_authorized', 'is_registered'])->group(function () {
 
 Route::get('companies', 'CompaniesController@index')->name('companies.index');
 Route::post('companies/{company_id}', 'CompaniesController@update')->name('companies.update')->middleware('my_company');
 Route::post('companies', 'CompaniesController@store')->name('companies.store');
 
-Route::get('cities', 'CitiesController@index')->name('cities.index');
-
 Route::get('branches', 'BranchesController@index')->name('branches.index');
 Route::post('branches', 'BranchesController@store')->name('branches.store');
 Route::post('branches/{branch_id}', 'BranchesController@update')->name('branches.update')->middleware('my_branch');
 Route::delete('branches/{branch_id}', 'BranchesController@destroy')->name('branches.destroy')->middleware('my_branch');
-
-Route::get('currencies', 'CurrenciesController@index')->name('currencies.index');
 
 });
