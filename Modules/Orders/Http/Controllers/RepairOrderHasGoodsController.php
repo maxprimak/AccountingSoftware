2db724dd1542351a471b60ed93ed3fd73f3f1ef8
maxappdev <maxym.oliynyk95@gmail.com>
2020-02-09 17:40:29 +0100

    debug

--- Modules/Orders/Http/Controllers/RepairOrderHasGoodsController.php
+++ Modules/Orders/Http/Controllers/RepairOrderHasGoodsController.php
@@ -57,9 +57,9 @@ class RepairOrderHasGoodsController extends Controller
                                 ->where('device_id', $request->device_id)
                                 ->where('warehouse_has_good_id', $warehouse_has_good['id'])->exists()){
                 $repair_order_has_good = new RepairOrderHasGood();
-                $repair_order_has_good = $repair_order_has_good->store($warehouse_has_good,$repair_order_id, $request->device_id);
-                //array_push($repair_order_has_goods,$repair_order_has_good);
-                //array_push($warehouse_has_good_ids,$warehouse_has_good['id']);
+                //$repair_order_has_good = $repair_order_has_good->store($warehouse_has_good,$repair_order_id, $request->device_id);
+                array_push($repair_order_has_goods,$repair_order_has_good);
+                array_push($warehouse_has_good_ids,$warehouse_has_good['id']);
             }
         }
         $warehouse_has_goods = WarehouseHasGood::whereIn('id',$warehouse_has_good_ids)->get();
@@ -72,7 +72,7 @@ class RepairOrderHasGoodsController extends Controller
         $repair_order_has_good = new RepairOrderHasGood();
         //$result_goods = $repair_order_has_good->combineGoodsRepairOrderHasGood($repair_order_has_goods,$goods);
 
-        return response()->json("here9");
+        return response()->json("here10");
         
         }
         return response()->json([]);
