fd1a8d4622014fea4a23f7be6db0ed47dcfc225e
maxappdev <maxym.oliynyk95@gmail.com>
2020-02-17 11:32:52 +0100

    git fix

--- Modules/Companies/Http/Middleware/MyCors.php
+++ Modules/Companies/Http/Middleware/MyCors.php
@@ -19,6 +19,6 @@ class MyCors
         return $next($request)
         ->header('Access-Control-Allow-Origin', 'http://localhost:8080') // maybe put this into the .env file so you can change the URL in production.
         ->header('Access-Control-Allow-Methods', '*') // or specify `'GET, POST, PUT, DELETE'` etc as the second parameter if you want to restrict the methods that are allowed.
-        ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization') // or add your headers.
+        ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization'); // or add your headers.
     }
 }
