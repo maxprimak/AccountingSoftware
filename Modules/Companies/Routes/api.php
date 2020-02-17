bc396fe67c287d671ffa963f060f007eab170b84
maxappdev <maxym.oliynyk95@gmail.com>
2020-02-17 11:13:30 +0100

    my cors middleware

--- Modules/Companies/Routes/api.php
+++ Modules/Companies/Routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 
 Route::middleware(['is_authorized'])->group(function () {
 
-Route::get('cities/{country_id}', 'CitiesController@index')->name('cities.index');
+Route::get('cities/{country_id}', 'CitiesController@index')->name('cities.index')->middleware('my_cors');
 Route::get('currencies', 'CurrenciesController@index')->name('currencies.index');
 Route::get('countries', 'CountriesController@index')->name('countries.index');
 
