286f27dc085ef9cd256555764a3cf6a14b367d39
maxappdev <maxym.oliynyk95@gmail.com>
2020-02-28 11:54:17 +0100

    bug show supplier price

--- Modules/Goods/Entities/Good.php
+++ Modules/Goods/Entities/Good.php
@@ -75,8 +75,11 @@ class Good extends Model
     }
 
     private function getSupplierPrice($supplier_order){
-      $has_prices = GoodHasPrices::where('supplier_id', $supplier_order->supplier_id)->where('good_id', $this->id)->first();
-      return $has_prices->retail_price;
+      $has_prices = GoodHasPrices::where('good_id', $this->id)
+                                  ->where('supplier_id', $supplier_order->supplier_id)
+                                  ->orWhere('supplier_id', null)
+                                  ->first();
+      return ($has_prices->retail_price == null) ? 0 : $has_prices->retail_price;
     }
 
     private function getWarehouse($order){
