6186c7d2cbf47870960330d7b0be70bf92aa7a0c
maxappdev <maxym.oliynyk95@gmail.com>
2020-03-05 12:01:20 +0100

    debug

--- Modules/Goods/Entities/GoodHasPrices.php
+++ Modules/Goods/Entities/GoodHasPrices.php
@@ -14,15 +14,16 @@ class GoodHasPrices extends Model
     {
 
         Log::info("out of loop");
-        
-        throw new \Exception("out of the loop. good has price:" . $good['retail_price'] . "isset:" . isset($good['retail_price']));
 
         if(isset($good['retail_price'])){
+
             $good_has_price = self::where('good_id',$good['good_id'])
                 ->where('branch_id',$branch_id)
                 ->where('supplier_id',$supplier_id)
                 ->orWhere('supplier_id',null)
                 ->first();
+
+            throw new \Exception("in the loop. good has price:" . $good_has_price);
             
             Log::info("in the loop");
 
