e90211bdac786a48765d3cf88a5a19c3e9ef8a76
Maxi <maxprimak1@gmail.com>
2020-11-10 20:58:01 +0100

    added current plan

--- Modules/Users/Transformers/UserResource.php
+++ Modules/Users/Transformers/UserResource.php
@@ -15,14 +15,23 @@ class UserResource extends Resource
      */
     public function toArray($request)
     {
+        $company = $this->user->company;
+        if ($company) {
+            $orders_left = $this->user->company->getOrdersLeft();
+            $current_plan = $this->user->company->getStripePlanName();
+        } else {
+            $orders_left = 0;
+            $current_plan = null;
+        }
         return [
             'email' => $this->email,
             'email_verified_at' => $this->email_verified_at,
             'id' => $this->id,
             'is_active' => $this->is_active,
             'username' => $this->username,
-            'company' => $this->user->company,
-            'orders_left' => $this->user->company->getOrdersLeft(),
+            'company' => $company,
+            'orders_left' => $orders_left,
+            'current_plan' => $current_plan,
 //            'comapny' => new CompanyResource($this->user->company)
 //            'person' => new PersonResource($this->user)
         ];
