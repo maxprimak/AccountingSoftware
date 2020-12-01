edfccb5d0e45d2c95b0608e459c628713ee9f5c1
Maxi <maxprimak1@gmail.com>
2020-12-01 08:30:46 +0100

    ENV URL FOR SIGN IN WITH GOOGLE

--- config/services.php
+++ config/services.php
@@ -49,7 +49,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost:8080/auth/google/callback',
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
     ],
 
 ];
